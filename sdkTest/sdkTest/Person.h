6ee4e2c5b55d25ca1911115f677f0f69c6e7b106
华云天下 <huayunworld@huayuntianxiademacbook-pro.local>
2016-11-18 10:13:31 +0800

    添加了person类

---
+++ sdkTest/sdkTest/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  sdkTest
+//
+//  Created by 华云天下 on 16/11/18.
+//  Copyright © 2016年 王兴隆. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ sdkTest/sdkTest/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  sdkTest
+//
+//  Created by 华云天下 on 16/11/18.
+//  Copyright © 2016年 王兴隆. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
